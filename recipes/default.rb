c5218421ea6516072e613126968470c33c889bfa
Kent Squires <ksquires@gmail.com>
2017-08-14 19:37:08 -0500

    working on fixing logging

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 #node.default['chef_client']['splay'] = '60'
 #node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
 #
-node.default['chef-client']['config']['log_level'] = :warn
+#node.default['chef-client']['config']['log_level'] = :warn
 include_recipe 'chef-client::default'
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
