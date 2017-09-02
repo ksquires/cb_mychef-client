b31daa31f9d9d34d66154abb9c6ca9ebb065ab0e
Kent Squires <ksquires@gmail.com>
2017-09-02 16:44:33 -0500

    changed 300 to 600 - chef doesn't need to run that often

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 #node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
 #
 node.default['chef-client']['config']['log_level'] = :auto
-node.default['chef_client']['interval'] = '300'
+node.default['chef_client']['interval'] = '600'
 node.default['chef_client']['splay'] = '5'
 include_recipe 'chef-client::default'
 include_recipe 'chef-client::config'
