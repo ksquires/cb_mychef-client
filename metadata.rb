d08dda64590fd2270471aae0722c04efb4fb1725
Kent Squires <ksquires@gmail.com>
2017-09-02 21:37:18 -0500

    metadata bump

--- metadata.rb
+++ metadata.rb
@@ -4,20 +4,20 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mychef-client'
 long_description 'Installs/Configures mychef-client'
-version '0.2.10'
+version '0.2.11'
 
 depends 'logrotate'
 depends 'chef-client', '>= 7.0.2'
-
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/mychef-client/issues' if respond_to?(:issues_url)
-
+issues_url 'https://github.com/ksquires/cb_mychef-client//issues' \
+if respond_to?(:issues_url)
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/mychef-client' if respond_to?(:source_url)
+source_url 'https://github.com/ksquires/mychef-client' \
+if respond_to?(:source_url)
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-#node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
+# node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
 #
 node.default['chef-client']['config']['log_level'] = :auto
 node.default['chef_client']['interval'] = '600'
@@ -12,4 +12,3 @@ node.default['chef_client']['splay'] = '5'
 include_recipe 'chef-client::default'
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
-
