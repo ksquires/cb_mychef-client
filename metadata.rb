b4307df53abf38221faddc8a65a4bbaedecb1b79
Kent Squires <ksquires@gmail.com>
2017-10-17 07:11:34 -0500

    dont need to run every 5m

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mychef-client'
 long_description 'Installs/Configures mychef-client'
-version '0.2.14'
+version '0.2.15'
 
 depends 'logrotate'
 depends 'chef-client', '>= 7.0.2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,8 @@
 # node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
 #
 #node.default['chef-client']['config']['log_level'] = :debug
-node.default['chef_client']['interval'] = '600'
-node.default['chef_client']['splay'] = '5'
+node.default['chef_client']['interval'] = '3600'
+node.default['chef_client']['splay'] = '30'
 include_recipe 'chef-client::default'
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
