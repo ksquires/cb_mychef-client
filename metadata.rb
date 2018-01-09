89c303466c748aa6e58cf4c863d21d8d6ccf6378
Kent Squires <ksquires@gmail.com>
2018-01-09 07:54:01 -0600

    loglevel setting

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mychef-client'
 long_description 'Installs/Configures mychef-client'
-version '0.2.15'
+version '0.2.16'
 
 depends 'logrotate'
 depends 'chef-client', '>= 7.0.2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,10 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 # node.default['chef_client']['log_file'] = '/var/log/chef/chef-client.log'
-#
-#node.default['chef-client']['config']['log_level'] = :debug
+node.default['chef_client']['log_file'] = 'chef-client.log'
 node.default['chef_client']['interval'] = '3600'
 node.default['chef_client']['splay'] = '30'
+node.default['chef-client']['config']['log_level'] = :auto
 include_recipe 'chef-client::default'
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
