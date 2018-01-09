a07f7d8ed8e6f84f70bbf6943961f9b0faa27c49
Kent Squires <ksquires@gmail.com>
2018-01-09 09:31:25 -0600

    removed audit stuff...

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,7 @@ client_config_hash = default['chef_client']['config'].to_hash
 
 client_config_hash.merge!(
   'log_level' => ':auto',
-  'log_location' => 'STDOUT',
-  'audit_mode' => 'enabled',
-  'enable_reporting' => 'true'
+  'log_location' => 'STDOUT'
 )
 
 default['chef_client']['config'] = client_config_hash
